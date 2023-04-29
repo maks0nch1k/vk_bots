030774281155ef9abd4315ee4eee2611bc0f019c
maks0nch1k <maksimf647@gmail.com>
2023-04-30 00:42:34 +0300

    task 2

---
+++ task 2.py
@@ -0,0 +1,31 @@
+import vk_api
+
+
+def main():
+    login, password = "+79819718551", "Maksx006"
+    vk_sess = vk_api.VkApi(login, password)
+    try:
+        vk_sess.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+    vkAPI = vk_sess.get_api()
+
+    friends = vkAPI.friends.get(fields="bdate")
+    if friends['items']:
+        answer = []
+        for user in friends['items']:
+            data = {"first_name": user["first_name"],
+                    "last_name": user["last_name"]}
+            if "bdate" in user:
+                data["date"] = user["bdate"]
+            else:
+                data["date"] = "дата рождения не указана"
+            answer.append(data)
+        answer.sort(key=lambda x: x["last_name"])
+        for friend in answer:
+            print(friend["last_name"], friend["first_name"], friend["date"])
+
+
+if __name__ == '__main__':
+    main()
